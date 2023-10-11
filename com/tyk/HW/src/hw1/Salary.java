c66ead2e5671c0709847d35f8140d7274ae77d9f
Vinskao <tianyikao@gmail.com>
2023-10-11 18:20:22 +0800

    salary done

--- com/tyk/HW/src/hw1/Salary.java
+++ com/tyk/HW/src/hw1/Salary.java
@@ -2,15 +2,17 @@ package hw1;
 
 public class Salary {
     public static void main(String[] args) {
-        int hours = 70;
+        int h = 100;
+        double standardRate = 150.0;
+        double otRate = 150.0 * 1.25;
+        double ootRate = 150.0 * 1.5;
 
-        if (hours <= 60) {
-            System.out.println("150");
-            if (hours <= 80)
-                System.out.println("150*1.25");
-
-
+        if (h <= 60) {
+            System.out.println(h * standardRate);
+        } else if (h <= 80){
+            System.out.println(60 * standardRate + (h - 60) * otRate);
+        } else {
+            System.out.println(60 * standardRate + (80 - 60) * otRate + (h-80) * ootRate);
         }
     }
-
 }
