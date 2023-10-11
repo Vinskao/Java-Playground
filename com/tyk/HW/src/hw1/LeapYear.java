eed2ddac87f37472220c74c870220604a0ab121c
Vinskao <tianyikao@gmail.com>
2023-10-11 19:40:37 +0800

    leap

---
+++ com/tyk/HW/src/hw1/LeapYear.java
@@ -0,0 +1,17 @@
+package hw1;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int y= 2300;
+        boolean isLeap = false;
+
+        if (y % 400 == 0){
+            isLeap = true;
+        } else if (y % 100 == 0){
+            isLeap = false;
+        } else if (y % 4 == 0){
+            isLeap = true;
+        }
+        System.out.println(isLeap);
+    }
+}
