6c17c94274087ae06d437295ba851f7dc56c79d7
Vinskao <tianyikao@gmail.com>
2023-10-11 18:01:12 +0800

    salary

---
+++ com/tyk/HW/src/hw1/Salary.java
@@ -0,0 +1,16 @@
+package hw1;
+
+public class Salary {
+    public static void main(String[] args) {
+        int hours = 70;
+
+        if (hours <= 60) {
+            System.out.println("150");
+            if (hours <= 80)
+                System.out.println("150*1.25");
+
+
+        }
+    }
+
+}
