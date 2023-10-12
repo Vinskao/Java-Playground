5346eb9820a96cb66dc3b712d3db0c297690cbca
Vinskao <tianyikao@gmail.com>
2023-10-12 11:32:11 +0800

    add 99

---
+++ com/tyk/Play/ProceduralLang/src/OneToTen.java
@@ -0,0 +1,11 @@
+public class OneToTen {
+    public static void main(String[] args) {
+        int n = 1;
+        int sum = 0;
+        while ( n <= 10){
+            sum += n;
+            n++;
+        }
+        System.out.println(sum);
+    }
+}
