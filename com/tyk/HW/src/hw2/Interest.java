6e9a1c1710cf2c98c6d81ee9d0f6fb25c87063bd
Vinskao <tianyikao@gmail.com>
2023-10-13 00:48:09 +0800

    interest

---
+++ com/tyk/HW/src/hw2/Interest.java
@@ -0,0 +1,21 @@
+package hw2;
+
+public class Interest {
+    public static void main(String[] args) {
+        double a;
+        double b;
+        int year = 1;
+        int p = 100000;
+        double ra = 0.1;
+        double rb = 0.05;
+        double suma = 0;
+        double sumb = 0;
+
+        while (!(p*(1+ra*year) < p*Math.pow((1+rb),year))){
+            year++;
+        }
+        System.out.println(year);
+        System.out.println("a的錢 " + p*(1+ra*year));
+        System.out.println("b的錢 " + p*Math.pow((1+rb),year));
+    }
+}
