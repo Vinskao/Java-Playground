7bcbd8f82808791cf23c410f73a8e24732303889
Vinskao <tianyikao@gmail.com>
2023-10-12 17:24:13 +0800

    factor

--- com/tyk/HW/src/hw2/Factor.java
+++ com/tyk/HW/src/hw2/Factor.java
@@ -1,12 +1,24 @@
 package hw2;
+import java.util.Arrays;
 
 public class Factor {
     public static void main(String[] args) {
-        int x = 0;
+        int x = 1024;
+        int[] f = new int[x]; //factors
+        int count = 0;
 
-        for (int i = 0; i < x; i++) {
-
+        for (int i = 1; i <= x; i++) {
+            if (x % i == 0) {
+                f[count] = i;
+                count++;
+            }
         }
+        //test
+        for (int i = 0; i < count; i++) {
+            if (f[i] != 0) {
+                System.out.print(f[i] + " ");
+            }
+        }
+        System.out.println(count);
     }
-
 }
