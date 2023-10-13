dd3a2384f6da5b773311f3e43de90127bcd5310d
Vinskao <tianyikao@gmail.com>
2023-10-13 17:31:42 +0800

    你的提交消息

--- com/tyk/Play/ProceduralLang/src/Overloading.java
+++ com/tyk/Play/ProceduralLang/src/Overloading.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Overloading {
+public class Overloading {
+
+    static void greeting(String name){
+        System.out.println("Hello! " + name);
+    }
+    static void greeting(String name,  int n){
+        for (int i = 0; i <= n; i++){
+            System.out.println("Hello! " + name);
+        }
+    }
+    public static void main(String[] args) {
+        greeting("Wavo");
+        greeting("Wavo", 3);
+
+    }
 }
