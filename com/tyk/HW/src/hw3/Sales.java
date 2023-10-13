d7c7e1836feeb72b801aa965bfea30294e82c0a7
Vinskao <tianyikao@gmail.com>
2023-10-13 19:02:43 +0800

    sales

--- com/tyk/HW/src/hw3/Sales.java
+++ com/tyk/HW/src/hw3/Sales.java
@@ -1,7 +1,34 @@
 package hw3;
-
 public class Sales {
-    public static void main(String[] args) {
+        public static int[] sum(int[] sales){
+            int[][] s =
+                    {
+                            {33, 32, 56, 45, 33},
+                            {77, 33, 68, 45, 23},
+                            {43, 55, 43, 67, 65}
+                    };
 
+            for (int i = 0; i < s.length ; i++){
+                int sum = 0;
+
+                for (int j = 0; j < s[i].length ; j++){
+                    sum += s[i][j];
+                }
+                sales[i] = sum;
+            }
+
+            for (int i = 0; i < s.length ; i++){
+                if (sales[i] > sales [i]){
+
+                }
+            }
+            return sales;
+        }
+    public static void main(String[] args) {
+        int[] personalSum = new int[3];
+        sum(personalSum);
+        System.out.println(personalSum[0]);
+        System.out.println(personalSum[1]);
+        System.out.println(personalSum[2]);
     }
 }
