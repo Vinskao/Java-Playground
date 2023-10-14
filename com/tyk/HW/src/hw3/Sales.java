aa49f0e556cdd19fd28c188b423c0846941b8e5e
Vinskao <tianyikao@gmail.com>
2023-10-14 20:22:06 +0800

    sales part1 add data in seperate method

--- com/tyk/HW/src/hw3/Sales.java
+++ com/tyk/HW/src/hw3/Sales.java
@@ -1,34 +1,32 @@
 package hw3;
 public class Sales {
-        public static int[] sum(int[] sales){
-            int[][] s =
-                    {
-                            {33, 32, 56, 45, 33},
-                            {77, 33, 68, 45, 23},
-                            {43, 55, 43, 67, 65}
-                    };
+
+        public static int[] sum(int[][] s){
+
+            int[] sales = new int[s.length];
 
             for (int i = 0; i < s.length ; i++){
                 int sum = 0;
-
                 for (int j = 0; j < s[i].length ; j++){
                     sum += s[i][j];
                 }
                 sales[i] = sum;
             }
-
-            for (int i = 0; i < s.length ; i++){
-                if (sales[i] > sales [i]){
-
-                }
-            }
             return sales;
         }
     public static void main(String[] args) {
-        int[] personalSum = new int[3];
-        sum(personalSum);
-        System.out.println(personalSum[0]);
-        System.out.println(personalSum[1]);
-        System.out.println(personalSum[2]);
+        int[][] salesData = {
+                {33, 32, 56, 45, 33},
+                {77, 33, 68, 45, 23},
+                {43, 55, 43, 67, 65}
+        };
+
+        String[] names = {"Joanne","Tom","Tina"};
+
+        int[] personalSum = sum(salesData);
+
+        for (int i = 0; i < salesData.length; i++) {
+            System.out.println(names[i]+" "+personalSum[i]);
+        }
     }
 }
