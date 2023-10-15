82e4b9f79b27ad9c35ef2d07004470db08de3776
Vinskao <tianyikao@gmail.com>
2023-10-15 16:39:04 +0800

    add Rain

---
+++ com/tyk/HW/src/hw3/Rain.java
@@ -0,0 +1,21 @@
+package hw3;
+import java.util.Scanner;
+public class Rain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("請輸入：");
+        String input = scanner.next();
+
+        if (input.equals("all")){
+        } else if (input.equals("year")){
+
+        } else if (input.equals("season")){
+
+        } else if (input.equals("month")){
+
+        } else {
+            System.out.println("error");
+        }
+    }
+}
