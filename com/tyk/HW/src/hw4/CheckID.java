2d759a512eaa513a425dde62ea596cd32472cc68
Vinskao <tianyikao@gmail.com>
2023-10-16 19:39:32 +0800

    checkId

--- com/tyk/HW/src/hw4/CheckID.java
+++ com/tyk/HW/src/hw4/CheckID.java
@@ -7,16 +7,38 @@ public class CheckID {
         this.hucoma = hucoma;
         this.value = value;
     }
-//    public transit(int){
-//
-//    }
     public static void main(String[] args) {
-        CheckID checkID = new CheckID("A","10");
+        CheckID[] checkIDArray = {
+                new CheckID("A","10"),
+                new CheckID("B","11"),
+                new CheckID("C","12"),
+                new CheckID("D","13"),
+                new CheckID("E","14"),
+                new CheckID("F","15"),
+                new CheckID("G","16"),
+                new CheckID("H","17"),
+                new CheckID("I","34"),
+                new CheckID("J","18"),
+                new CheckID("K","19"),
+                new CheckID("M","21"),
+                new CheckID("N","22"),
+                new CheckID("O","35"),
+                new CheckID("P","23"),
+                new CheckID("Q","24"),
+                new CheckID("T","27"),
+                new CheckID("U","28"),
+                new CheckID("V","29"),
+                new CheckID("W","32"),
+                new CheckID("X","30"),
+                new CheckID("Z","33"),
+        };
+
         boolean isID = false;
-        String id = "f123436739";
+        String id = "A192321673";
+        String inputHucoma = id.substring(0,1);
         if (id.length() != 10){
             System.out.println("Invalid ID");
-        } else if ((id.charAt(0) < 'A' || id.charAt(0) > 'Z') && (id.charAt(0) < 'a' || id.charAt(0) > 'z')){
+        } else if ((id.charAt(0) < 'A' || id.charAt(0) > 'Z')){
             System.out.println("Invalid ID");
         } else if (id.charAt(1) != '1' && id.charAt(1) != '2'){
             System.out.println("Invalid ID");
@@ -27,11 +49,33 @@ public class CheckID {
                     return;
                 }
             }
-            if (id.charAt(9) == '0'){
-
+            String getValue = null;
+            for (CheckID object : checkIDArray){
+                if(object.hucoma.equals(inputHucoma)){
+                    getValue = object.value;
+                    break;
+                }
             }
-            isID = true;
-            System.out.println("Valid ID" + isID);
+            int S = 0;
+            S = Integer.parseInt(getValue.substring(0, 1),10) +
+                Integer.parseInt(getValue.substring(1, 2),10) * 9 +
+                Integer.parseInt(id.substring(1,2),10) * 8 +
+                Integer.parseInt(id.substring(2,3),10) * 7 +
+                Integer.parseInt(id.substring(3,4),10) * 6 +
+                Integer.parseInt(id.substring(4,5),10) * 5 +
+                Integer.parseInt(id.substring(5,6),10) * 4 +
+                Integer.parseInt(id.substring(6,7),10) * 3 +
+                Integer.parseInt(id.substring(7,8),10) * 2 +
+                Integer.parseInt(id.substring(8,9),10) * 1
+            ;
+            int R = S % 10;
+            String checkMa = Integer.toString(10 - R);
+            if (id.substring(9, 10).equals(checkMa)){
+                isID = true;
+                System.out.println("ID is " + isID);
+            } else {
+                System.out.println("Fake ID");
+            }
         }
     }
 }
