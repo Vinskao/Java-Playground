c7ae9522e2770e586cf1526f259f0a675b66e411
Vinskao <tianyikao@gmail.com>
2023-10-17 15:15:15 +0800

    提交本地修改

--- com/tyk/Play/ProceduralLang/src/Animal/Dog.java
+++ com/tyk/Play/ProceduralLang/src/Animal/Dog.java
@@ -1,2 +1,10 @@
-package Animal;public class Dog {
+package Animal;
+
+public class Dog extends Animal{
+    public void run(){
+        System.out.println("Dog is running");
+    }
+    public void bark(){
+        System.out.println("Dog is barking");
+    }
 }
--- com/tyk/Play/ProceduralLang/src/Animal/Test2.java
+++ com/tyk/Play/ProceduralLang/src/Animal/Test2.java
@@ -1,20 +1,10 @@
 package Animal;
 
-public class Test {
+public class Test2 {
     public static void main(String[] args) {
-        Animal animal = new Animal();
-        animal.run();
-        Cat cat = new Cat();
-        cat.run();
-        animal = cat;
-        animal.run();
-
-        Animal[] animals = new Animal[3];
-        animals[0] = new Feline();
-        animals[1] = new Cat();
-        animals[2] = new Animal();
-        for (Animal ani : animals){
-            ani.run();
-        }
+        Animal animal = new Dog();
+//        animal.bark()
+        Dog dog = (Dog)animal;
+        dog.bark();
     }
 }
