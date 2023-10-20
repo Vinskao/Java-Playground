914f7f82d06aca9ca6aacbfbab0000e62681f0a9
Vinskao <tianyikao@gmail.com>
2023-10-20 17:56:44 +0800

    stack throw

--- com/tyk/HW/src/hw5/Stack/Stack.java
+++ com/tyk/HW/src/hw5/Stack/Stack.java
@@ -2,9 +2,17 @@ package hw5.Stack;
 
 public class Stack {
     private int[] saveData;
-    private int top;
-    public void push(int data){ //放資料
-
+    private int top; //最上層資料的註標值
+    public Stack(int size){
+        saveData = new int[size];
+        top = -1;
+    }
+    public void push(int data) throws StackFullException{ //放資料
+        if(top == saveData.length -1){
+            saveData[++top] = data; //top先+1後在saveData[]遍歷
+        } else {
+            throw new StackFullException();
+        }
     }
     public int pop(int data){ //取資料
         return data;
